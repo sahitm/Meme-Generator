e02923691f753b29e94bd03196fb5478f3c8a1a1
sahitm <sahit7varma@gmail.com>
2022-06-14 15:42:36 +0530

    style -- added style to the form jsx in meme component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,8 @@ import logo from "../images/troll-face.png"
 export default function Header(){
     return(
         <section className="header">
-            <div className="logo--name">
-                <img src={logo} alt="troll face"></img>
-                <h1 className="app--name">Meme Generator</h1>
-            </div>
+            <img className="header--logo"  src={logo} alt="troll face"></img>
+            <h1 className="header--title">Meme Generator</h1>
             <h6>React Course - Project 3</h6>
         </section>
     )
